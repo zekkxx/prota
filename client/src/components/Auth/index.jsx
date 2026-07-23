892a96739a654e7a4d3e4c5c3c3d990cbbbde04e
Kieran Anthony <zekkxx@gmail.com>
2026-07-23 07:34:16 -0500

    Let's try removing the process and placing the end point in plain view. I hate this but I am running out of time.

--- client/src/components/Auth/index.jsx
+++ client/src/components/Auth/index.jsx
@@ -1,13 +1,15 @@
 import "./style.css";
 
-import path from "path";
-
 const Auth = () => {
-  console.log("process.env.NODE_ENV", process.env.AXIOS_BASE_URL);
-  const authPath = process.env.NODE_ENV === "production" ? path.join(process.env.AXIOS_BASE_URL, "./auth/github") : "http://localhost:3001/auth/github";
   return (
     <div className="auth-container">
-      <a href={authPath} rel="noopener noreferrer">
+      <a
+        href={
+          process.env.NODE_ENV === "production"
+            ? "https://prota.onrender.com/auth/github"
+            : "http://localhost:3001/auth/github"
+        }
+      >
         <div className="github-button">Sign in with Github</div>
       </a>
     </div>
