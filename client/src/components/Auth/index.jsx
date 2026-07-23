bcc654f2ef19c56b23eeb3242946737e635d550b
Kieran Anthony <zekkxx@gmail.com>
2026-07-23 07:29:59 -0500

    I have to head out the door in a few minutes please let this work.

--- client/src/components/Auth/index.jsx
+++ client/src/components/Auth/index.jsx
@@ -3,15 +3,11 @@ import "./style.css";
 import path from "path";
 
 const Auth = () => {
+  console.log("process.env.NODE_ENV", process.env.AXIOS_BASE_URL);
+  const authPath = process.env.NODE_ENV === "production" ? path.join(process.env.AXIOS_BASE_URL, "./auth/github") : "http://localhost:3001/auth/github";
   return (
     <div className="auth-container">
-      <a
-        href={
-          process.env.NODE_ENV === "production"
-            ? path.join(process.env.AXIOS_BASE_URL, "./auth/github")
-            : "http://localhost:3001/auth/github"
-        }
-      >
+      <a href={authPath} rel="noopener noreferrer">
         <div className="github-button">Sign in with Github</div>
       </a>
     </div>
